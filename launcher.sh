c1537a42e98b3a64c459845fc82820567cd77b91
peopleassistant <peopleassistant@gmail.com>
2025-09-27 17:26:07 +0800

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -84,7 +84,7 @@ if [ -e $CURRENT_VERSION/panel/database.json -a ! -e database/database.json ]; t
     ln -s ../../database/database.json .
     cd ../..
 fi
-LATEST_VERSION=v1.34-beta
+LATEST_VERSION=v1.35
 if [ $CURRENT_VERSION != $LATEST_VERSION ]; then
     mkdir -p $LATEST_VERSION
     cd $LATEST_VERSION
